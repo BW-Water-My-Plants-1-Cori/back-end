65d347dcc068995a2711f6201af383612d3b48c2
Aveline Wingfield <avelinewingfield@gmail.com>
2020-09-18 18:42:56 -0400

    Knex Setup

--- knexfile.js
+++ knexfile.js
@@ -4,10 +4,38 @@ module.exports = {
 
   development: {
     client: 'sqlite3',
+    useNullAsDefault: true, 
     connection: {
-      filename: './dev.sqlite3'
-    }
+      filename: './data/plantbase.db3',
+    },
+    migrations: {
+      directory: '.data/migrations'
+    },
+    seeds:{
+      directory: '.data/seeds'
+    },
+    // needed when using foreign keys
+    pool: {
+      afterCreate: (conn, done) => {
+        // runs after a connection is made to the sqlite engine
+        conn.run('PRAGMA foreign_keys = ON', done); // turn on FK enforcement
+      },
+    },
   },
+  
+   testing: {
+      client: 'sqlite3',
+      connection: {
+        filename: './data/test.db3',
+      },
+      useNullAsDefault: true,
+      migrations: {
+        directory: './data/migrations',
+      },
+      seeds: {
+        directory: './data/seeds',
+      },
+    },  
 
   staging: {
     client: 'postgresql',
