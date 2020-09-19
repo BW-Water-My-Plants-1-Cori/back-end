48be71e14aa368f0a3f76509bc1478218467f018
Aveline Wingfield <avelinewingfield@gmail.com>
2020-09-18 22:06:24 -0400

    Login/Registration Routed

--- index.js
+++ index.js
@@ -5,23 +5,15 @@ const helmet = require('helmet')
 
 const users = require('./users/usersRoute')
 const plants = require('./plants/plantsRoute')
+const auth = require('./auth/server')
 const server = express()
 
-server.use(cors(), express.json(), helmet(), morgan())
+server.use(cors(), express.json(), helmet(), morgan('dev'))
 server.use('/users', users)
 server.use('/plants', plants)
+server.use('/', auth)
 
-server.get('/', (req, res)=>{
-    res.status(200).json({api: "Up"})
-})
 
-server.post('/register', (req, res)=>{
-    
-})
-
-server.post('/login', (req, res)=>{
-
-})
 
 const PORT = process.env.PORT || 4000
 
