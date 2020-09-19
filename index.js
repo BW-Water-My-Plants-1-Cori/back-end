9119b3820f9c8cc2234900937afc3d56673f0d64
Aveline Wingfield <avelinewingfield@gmail.com>
2020-09-18 22:02:19 -0400

    Routes routed

--- index.js
+++ index.js
@@ -3,12 +3,24 @@ const cors = require('cors')
 const morgan = require('morgan')
 const helmet = require('helmet')
 
+const users = require('./users/usersRoute')
+const plants = require('./plants/plantsRoute')
 const server = express()
 
 server.use(cors(), express.json(), helmet(), morgan())
+server.use('/users', users)
+server.use('/plants', plants)
 
 server.get('/', (req, res)=>{
     res.status(200).json({api: "Up"})
+})
+
+server.post('/register', (req, res)=>{
+    
+})
+
+server.post('/login', (req, res)=>{
+
 })
 
 const PORT = process.env.PORT || 4000
---
+++ users/usersRoute.js
@@ -0,0 +1,27 @@
+const express = require('express')
+
+const router = express.Router()
+
+
+
+router.get('/:id', (req, res)=>{
+
+})
+
+router.get('/:id/plants',(req, res)=>{
+
+})
+
+router.post('/:id/plants', (req, res)=>{
+
+})
+
+router.put('/:id', (req, res)=>{
+    
+})
+
+router.delete('/:id', (req, res)=>{
+    
+})
+
+module.exports = router
